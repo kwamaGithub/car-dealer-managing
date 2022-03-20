80432049eb91730ca570eb69bcc518812a32f4e2
kdjimissa <djimwama@gmail.com>
2022-03-20 01:16:35 +0000

    RestController completed first Part

--- src/main/java/com/agency/restcontroller/ListingRestController.java
+++ src/main/java/com/agency/restcontroller/ListingRestController.java
@@ -5,14 +5,18 @@
  */
 package com.agency.restcontroller;
 
+import com.agency.DTO.ListingDTO;
 import com.agency.service.ManagingService;
 import com.agency.utils.AppConstant;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,6 +28,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = AppConstant.MANAGING_REST_PATH_API)
 public class ListingRestController {
 
+    private static final String DEALERS = "dealers";
+    private static final String LISTINGS = "listings";
+    private static final String LISTING_DTO = "listingDTO";
+
     @Autowired
     private ManagingService managingService;
 
@@ -31,11 +39,62 @@ public class ListingRestController {
      *
      * @return
      */
-    @RequestMapping(value = "/getAllDealer", method = RequestMethod.GET)
-    public Object loadFonction() {
+    @GetMapping(value = "/getAllDealer")
+    public Object getDealers() {
         Map<String, Object> p = new HashMap<>();
-        p.put("dealer", this.managingService.getAllDealer());
+        p.put(DEALERS, this.managingService.getAllDealer());
         return p;
     }
 
+    /**
+     *
+     * @param dealerId
+     * @param stateId
+     * @return
+     */
+    @GetMapping(value = "/getDealerListing/{dealerId}/{stateId}")
+    public Object getDealerListingByState(@PathVariable("dealerId") String dealerId,
+            @PathVariable("stateId") String stateId) {
+        Map<String, Object> p = new HashMap<>();
+        p.put(LISTINGS, this.managingService.getDealerListingByState(dealerId, stateId));
+        return p;
+    }
+
+    /**
+     *
+     * @param listingDTO
+     * @return
+     */
+    @PutMapping(value = "/updateListing")
+    public Object updateListing(@RequestBody() ListingDTO listingDTO) {
+        Map<String, Object> p = new HashMap<>();
+        this.managingService.updateListing(listingDTO);
+        p.put("listingEdited", true);
+        p.put("listingId", listingDTO.getId());
+        return p;
+    }
+
+    /**
+     *
+     * @param listingDTO
+     * @return
+     */
+    @PostMapping(value = "/saveListing")
+    public Object saveListing(@RequestBody() ListingDTO listingDTO) {
+        Map<String, Object> p = new HashMap<>();
+        p.put(LISTING_DTO, this.managingService.createListing(listingDTO));
+        return p;
+    }
+
+    /**
+     *
+     * @param listingId
+     * @return
+     */
+    @PutMapping(value = "/publishedOrUnpublish/{listingId}")
+    public Object updateListing(@PathVariable("listingId") String listingId) {
+        Map<String, Object> p = new HashMap<>();
+        p.put(LISTING_DTO, this.managingService.publishOrUnpublishListing(listingId));
+        return p;
+    }
 }
